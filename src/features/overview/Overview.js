34dc421ba6994d5c83779b860682ec1cc8fa541e
Wolk9 <info@setisbew.nl>
2022-07-24 01:27:14 +0200

    .

--- src/features/overview/Overview.js
+++ src/features/overview/Overview.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Card, Container } from "react-bootstrap";
 
 export const Overview = () => {
-  return <div>Overview</div>;
+  return (
+    <div>
+      <Container fluid className="p-4 m4">
+        <Card xs={4}>
+          <Card.Header as="h5">Overview</Card.Header>
+          <Card.Body>lorem ipsum dolor sit amet, consectet</Card.Body>
+          <Card.Footer></Card.Footer>
+        </Card>
+      </Container>
+    </div>
+  );
 };
 
 export default Overview;
