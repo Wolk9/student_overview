8c66f99b386ea188e693714eb99ec35198d6da0d
Wolk9 <info@setisbew.nl>
2022-07-27 22:47:20 +0200

    Students list in sortable DataTable

--- src/features/students/StudentsList.js
+++ src/features/students/StudentsList.js
@@ -1,13 +1,46 @@
 import React from "react";
 import { Table, Button, Col, Card } from "react-bootstrap";
-import { MDBContainer } from "mdb-react-ui-kit";
+import { MDBContainer, MDBCol, MDBRow } from "mdb-react-ui-kit";
+import styled, { keyframes } from "styled-components";
 import { useSelector } from "react-redux";
 import StudentItem from "./StudentItem";
 import { useDispatch } from "react-redux";
 import { openAddStudentModal } from "../ui/uiSlice";
+import { editStudent } from "./studentSlice";
 import AddStudentModal from "./AddStudentModal";
-import { mdiAccountPlus } from "@mdi/js";
+import { mdiAccountPlus, mdiPencil } from "@mdi/js";
 import Icon from "@mdi/react";
+import DataTable from "react-data-table-component";
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Spinner = styled.div`
+  margin: 16px;
+  animation: ${rotate360} 1s linear infinite;
+  transform: translateZ(0);
+  border-top: 2px solid grey;
+  border-right: 2px solid grey;
+  border-bottom: 2px solid grey;
+  border-left: 4px solid black;
+  background: transparent;
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+`;
+
+const CustomLoader = () => (
+  <div style={{ padding: "24px" }}>
+    <Spinner />
+  </div>
+);
 
 const StudentsList = ({ students, show }) => {
   const dispatch = useDispatch();
@@ -17,14 +50,75 @@ const StudentsList = ({ students, show }) => {
     dispatch(openAddStudentModal(true));
   };
 
+  const funColor = students.color;
+  const difficultyColor = students.color2;
+
+  const handleEditClick = (e) => {
+    console.log("Click on StudentEdit " + e.id + " happend");
+    dispatch(editStudent(e.id));
+  };
+
+  const columns = [
+    {
+      name: "Name",
+      selector: (row) => row.firstName + " " + row.lastName,
+      sortable: true,
+      width: "40%",
+    },
+    {
+      name: "Phone",
+      selector: (row) => row.phone,
+      sortable: true,
+    },
+    {
+      name: "Email",
+      selector: (row) => row.email,
+      sortable: true,
+    },
+    {
+      name: "Colors",
+      selector: (row) => (
+        <MDBRow>
+          <MDBCol>
+            <div className="swatch" style={{ backgroundColor: row.color }} />
+          </MDBCol>
+          <MDBCol>
+            <div className="swatch" style={{ backgroundColor: row.color2 }} />
+          </MDBCol>
+        </MDBRow>
+      ),
+      width: "10%",
+    },
+    {
+      name: "",
+      selector: (row) => (
+        <Icon
+          path={mdiPencil}
+          size={1}
+          key={row.id}
+          onClick={(e) => handleEditClick(e)}
+        />
+      ),
+      width: "10%",
+    },
+  ];
+
   return (
     <div>
       {show && <AddStudentModal show={show} students={students} />}
       <MDBContainer fluid className="p-4 m4">
-        <Card xs={4}>
-          <Card.Header as="h5">List of Students</Card.Header>
-          <Card.Body>
-            <Table striped="columns" size="sm" className="p-4 m4">
+        <DataTable
+          title={students.length + " Students"}
+          columns={columns}
+          data={students}
+          pagination
+          dense
+          responsive
+          progressComponent={<CustomLoader />}
+          onRowClicked={(e) => handleEditClick(e)}
+        />
+
+        {/* <Table striped="columns" size="sm" className="p-4 m4">
               <thead>
                 <tr>
                   <th>Name</th>
@@ -55,12 +149,7 @@ const StudentsList = ({ students, show }) => {
                   />
                 ))}
               </tbody>
-            </Table>
-          </Card.Body>
-          <Card.Footer className="text-muted">
-            A total of {students.length} students registered
-          </Card.Footer>
-        </Card>
+            </Table> */}
       </MDBContainer>
     </div>
   );
